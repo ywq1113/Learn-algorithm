d1030e28db3c867b4639aadc84f3db516def63e0
ywq1113 <419083353@qq.com>
2019-12-23 22:11:42 +0800

    Z字形变换

---
+++ leetcode/6/READEME.md
@@ -0,0 +1,14 @@
+这道题需要掌握vector<string>的一些知识
+
+string s;
+
+string的成员函数length()等同于成员函数size(),返回字符个数(无符号数)，需要对其进行类型转换  
+如(int)s.size()或(int)s.length
+
+对字符串s进行遍历  
+for(char c : s)
+
+对字符串列表进行遍历
+for(row : rows)
+
+
--- leetcode/6/convert.cpp
+++ leetcode/6/convert.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdio>
 
 using namespace std;
 
@@ -16,17 +17,46 @@ using namespace std;
 /*思路
  * 从左至右迭代字符串
  * 确定字符位于Z字形图案的哪一行
+ * 构建字符串列表
+ * 遍历，将字符串列表每一行加起来
  */
 
 class Solution{
 public:
-    string convert(sting s, int numRows)
+    string convert(string s, int numRows)
     {
         if(numRows == 1)  return s;
-        
-        vector<string> rows(min(numRows, int(s.size())));
-        int curRow=0
-        bool goingDown = false;
 
+        int n = (int)s.size();
+        int curRow = 0;
+        bool godown = false;
+        
+        vector<string> rows(min(numRows, n));
         for(char c : s){
-            
+            rows[curRow] += c;
+            if(curRow==0 || curRow == numRows-1)  godown = !godown;
+            curRow += godown ? 1 : -1;
+          /*等价于
+           *if(godown)  curRow -= 1;
+            else  curRow += 1;
+           */
+        }
+
+        string res;
+        for(string row : rows){
+            res += row;
+        }
+        return res;
+    }
+};
+
+int main(){
+    Solution sol;
+    string s("leetcode");
+    int num;
+    printf("Enter the number of rows:");
+    scanf("%d", &num);
+    string res = sol.convert(s, num);
+    //printf("%s", res);
+    cout << res << endl;
+}  
