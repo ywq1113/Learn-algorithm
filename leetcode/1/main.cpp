298da610039140a07450cc6727e4496c4e600097
ywq1113 <419083353@qq.com>
2020-01-14 20:46:20 +0800

    两数之和

---
+++ leetcode/1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include "twoSum.h"
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> hash;
+    vector<int> vec;
+    for(unsigned int i=0;i<nums.size();++i)
+    {
+        int complement = target - nums[i];
+        if(hash.find(complement)!=hash.end()){
+            vec.push_back(i);
+            vec.push_back(hash[complement]);
+            return vec;
+        }
+        hash[nums[i]] = i;
+    }
+    return vec;
+}
+
+
+int main()
+{
+    vector<int> nums{2,7,11,15};
+    int target = 11;
+    Solution s;
+    vector<int>* p = &s.twoSum(nums, target);
+    vector<int> :: iterator iter;
+    for(iter=p->begin();iter!=p->end();++iter)
+        cout<< *iter << endl;
+    return 0;
+}
---
+++ leetcode/1/twoSum.h
@@ -0,0 +1,13 @@
+#ifndef  TWO_SUM_H
+#define  TWO_SUM_H
+
+class Solution{
+public:
+    Solution();
+    std::vector<int> twoSum(std::vector<int> &nums, int target);
+    ~Solution();
+private:
+    
+};
+
+#endif
